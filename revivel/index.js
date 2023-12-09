b3be49c1cbb12ca13641978395c6795a48e9f314
LuanSantos <luansantos@outlook.com>
2023-12-09 15:36:40 -0300

    app to do list
    in development

---
+++ revivel/index.js
@@ -0,0 +1,8 @@
+const addTaskbtn = document.getElementById("add-task")
+const tasks = document.querySelector('#tasks')
+
+
+addTaskbtn.addEventListener('click', function addTask => () {
+    const task = document.createElement('p')
+    
+})
